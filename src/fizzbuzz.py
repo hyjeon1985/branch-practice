d196d37a7864e7726ef94c26536d331bc92b29e7
J.Hoya <hyjeon1985@gmail.com>
2025-12-18 10:29:29 +0900

    feat(fizzbuzz): Develope fizz.

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,5 +1,9 @@
-def fizzbuzz():
-    print("hello")
+def fizzbuzz(start: int=1, end: int=15):
+    for i in range(start, end + 1):
+        if i % 3 == 0:
+            print("fizz")
+        else:
+            print(i)
 
 
 if __name__ == "__main__":
