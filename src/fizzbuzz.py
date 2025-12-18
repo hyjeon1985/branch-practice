251bc6f0eb36b4c8ff45441261b60e154f811c83
J.Hoya <hyjeon1985@gmail.com>
2025-12-18 10:57:55 +0900

    feat(fizzbuzz): Develope FizzBuzz one-liner.

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,13 +1,14 @@
 def fizzbuzz(start: int=1, end: int=15):
     for i in range(start, end + 1):
-        if i % 15 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+#        if i % 15 == 0:
+#            print("FizzBuzz")
+#        elif i % 3 == 0:
+#            print("Fizz")
+#        elif i % 5 == 0:
+#            print("Buzz")
+#        else:
+#            print(i)
+        print(f"{'Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0) or i}")
 
 if __name__ == "__main__":
     fizzbuzz()
